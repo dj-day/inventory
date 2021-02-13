1bea35fbb739c6f58d0658bf378e7ecc3b24b8b8
dj-day <71570613+dj-day@users.noreply.github.com>
2021-02-13 18:29:57 -0500

    updated dataservice
    changed dataservice to connection string for heroku url

--- app/data/dataservice.js
+++ app/data/dataservice.js
@@ -1,11 +1,7 @@
 const Database = require('pg').Pool;
 
 const connection = new Database({
-    user: process.env.USER || 'inv',
-    host: process.env.HOST || 'localhost',
-    database: process.env.DATABASE || 'inventory',
-    password: process.env.PASSWORD || 'password12345',
-    port: process.env.PORT || 5432,
+    connectionString: process.env.DATABASE_URL || 'postgresql://inv:password12345@localhost:5432/inventory'
   });
 
   module.exports = {
