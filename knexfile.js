e95f9f94bafc20e2d36a12de5d8908d8a444280c
dj-day <71570613+dj-day@users.noreply.github.com>
2021-02-13 23:30:05 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
 
   staging: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL+'?ssl=true',
     pool: {
       min: 2,
       max: 10
@@ -21,7 +21,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL+'?ssl=true',
     pool: {
       min: 2,
       max: 10
