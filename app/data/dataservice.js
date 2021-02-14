3cfedc1e0c992368aebab59bd2e3fbd626c042b7
dj-day <71570613+dj-day@users.noreply.github.com>
2021-02-13 22:03:35 -0500

    Update dataservice.js

--- app/data/dataservice.js
+++ app/data/dataservice.js
@@ -1,7 +1,10 @@
 const Database = require('pg').Pool;
 
 const connection = new Database({
-    connectionString: process.env.DATABASE_URL || 'postgresql://inv:password12345@localhost:5432/inventory'
+    connectionString: process.env.DATABASE_URL || 'postgresql://inv:password12345@localhost:5432/inventory',
+    ssl: {
+        rejectUnauthorized: false
+      }
   });
 
   module.exports = {
