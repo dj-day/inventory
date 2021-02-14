138f19ceace568ce70601ba03f6407d07fc3603d
dj-day <71570613+dj-day@users.noreply.github.com>
2021-02-13 21:08:30 -0500

    switching port
    switched port to env variable

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const dataService = require('./data/dataservice');
 const CONSTANTS = require('./constants/constants');
 const QUERIES = require('./data/queries');
