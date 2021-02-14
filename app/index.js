90f4e258c41b0d5a6389c3a44bf09425cf63f757
dj-day <71570613+dj-day@users.noreply.github.com>
2021-02-13 21:39:06 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -48,5 +48,5 @@ app.post('/inventory', body('name').isString(), function(req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
